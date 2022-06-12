90ac91b017463d90adf8a9abe7337dc190bb7342
Mariusz <mariuszbrozda.dublin@gmail.com>
2022-06-12 20:17:03 +0100

    Add exception incase file is not found

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ BACKGROUND_COLOR = "#B1DDC6"
 to_learn = {}
 current_card = {}
 
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
-
-# FUNCTION TO PISK NEXT CARD
+# FUNCTION TO PICK NEXT CARD
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -33,19 +40,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(4000, func='')
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="../../../Downloads/Solution+-+flash-card-project-end/images/card_front.png")
-card_back_img = PhotoImage(file="../../../Downloads/Solution+-+flash-card-project-end/images/card_back.png")
+card_front_img = PhotoImage(file="./images/card_front.png")
+card_back_img = PhotoImage(file="./images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
-cross_image = PhotoImage(file="../../../Downloads/Solution+-+flash-card-project-end/images/wrong.png")
+cross_image = PhotoImage(file="./images/wrong.png")
 unknown_button = Button(image=cross_image, highlightthickness=0, command='')
 unknown_button.grid(row=1, column=0)
 
-check_image = PhotoImage(file="../../../Downloads/Solution+-+flash-card-project-end/images/right.png")
+check_image = PhotoImage(file="./images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command='')
 known_button.grid(row=1, column=1)
 
