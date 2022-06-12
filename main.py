2ed491d640504abced4dbc3baadb2a90206bd891
Mariusz <mariuszbrozda.dublin@gmail.com>
2022-06-12 20:24:17 +0100

    Add function to creating list of words left to learn

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def next_card():
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     canvas.itemconfig(card_background, image=card_front_img)
-
+    flip_timer = window.after(4000, func=flip_card)
 
 # FLIP CARD FUNCTION
 def flip_card():
@@ -30,13 +30,21 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+# SAVE WORDS THAT ARE ALREADY LEARNT AND CREATE LIST OF WORDS TO LEARN
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
 
 # CREATE GUI
 window = Tk()
 window.title("PoliFino Flash cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer = window.after(4000, func='')
+flip_timer = window.after(4000, func=flip_card)
 
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="./images/card_front.png")
@@ -48,13 +56,13 @@ canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="./images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0, command='')
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="./images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command='')
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
-
+next_card()
 
 window.mainloop()
